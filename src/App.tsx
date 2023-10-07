2cb0423062bbf9a80f6b737e8d0b90915f989444
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-07 00:36:06 -0300

    feat(comp): adds Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import Transactions from './pages/Transactions'
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>hefgasdfasllo</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
