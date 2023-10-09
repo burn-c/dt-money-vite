8655c70610f4ea4cfd01294f5bd0fad1548f54b2
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-08 23:43:07 -0300

    feat(TransactionTypeButton): adds raio button props and accessibility

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -3,6 +3,7 @@ import {
   DialogContent,
   DialogOverlay,
 } from '@radix-ui/react-dialog'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 import styled from 'styled-components'
 
 export const Overlay = styled(DialogOverlay)`
@@ -71,7 +72,7 @@ export const CloseButton = styled(DialogClose)`
   color: ${(props) => props.theme['gray-500']};
 `
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
@@ -81,7 +82,9 @@ export const TransactionType = styled.div`
 interface TransactionTypeButtonProps {
   variant: 'income' | 'outcome'
 }
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(
+  RadioGroup.Item,
+)<TransactionTypeButtonProps>`
   background: ${(props) => props.theme['gray-700']};
   padding: 1rem;
   display: flex;
@@ -97,5 +100,22 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
       props.variant === 'income'
         ? props.theme['green-300']
         : props.theme['red-300']};
+  }
+
+  &[data-state='unchecked']:hover {
+    background: ${(props) => props.theme['gray-600']};
+    transition: background-color 0.2s;
+  }
+
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-500']
+        : props.theme['red-500']};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
   }
 `
